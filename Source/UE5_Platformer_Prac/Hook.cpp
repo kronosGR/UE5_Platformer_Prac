ff1941c094584ddd7132f54d954acef797520650
Katsanakis Georgios <geo.elgeo@gmail.com>
2022-07-03 07:23:31 +0200

    started hook actorcomponent

---
+++ Source/UE5_Platformer_Prac/Hook.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Hook.h"
+
+// Sets default values for this component's properties
+UHook::UHook()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UHook::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UHook::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
---
+++ Source/UE5_Platformer_Prac/UE5_Platformer_Prac.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UE5_Platformer_Prac.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE5_Platformer_Prac, "UE5_Platformer_Prac" );
